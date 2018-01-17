90982864630f6175c6b53d63c4a6426e36b7b251
Vatsal Savani <30824146+vatsalsavani@users.noreply.github.com>
2018-01-17 19:24:43 +0800

    Random Image Generator Discord Bot
    Uploaded discord bot to upload random images based of a search term

---
+++ DiscordBot_RandomImage/bot_code.py
@@ -0,0 +1,55 @@
+#Vatsal Savani
+#Random Image Uploader Discord Bot
+#A discord bot that retrieves and sends a random image based of a search term given with a command
+
+
+import discord, random, os
+from discord.ext.commands import Bot
+from discord.ext import commands
+
+#Setting up connection
+Client = discord.Client()
+botPrefix = '>'
+client = commands.Bot(command_prefix = botPrefix)
+
+#Do the following things once bot is ready
+@client.event	
+async def on_ready () :
+	print('Bot Online!')
+	print('Name : {}'.format(client.user.name))
+	print('ID : {}'.format(client.user.id))
+
+
+
+
+images  = [[], []]
+
+#Gets all images from a directory named ImagePool and the categories within it.
+for file in os.listdir('.\ImagePool/') :
+	images[0].append(file)
+	images[1].append(os.listdir('./ImagePool/'+ file))
+
+
+@client.command(pass_context = True)
+async def randompic (ctx, search: str) :
+
+	#Uploads random image if search term is part of imagepool categories
+	if search in images[0] :
+		CindexNum = images[0].index(search)
+		category = search
+		imageName = images[1][CindexNum][random.randint(0, len(images[1][CindexNum]) - 1)]
+
+	#If search term is not found, it uploads a random image from all
+	else :	
+		await client.say ('Sorry no such category, showing random image!')
+		CindexNum = random.randint(0, len(images[0]))
+		category = images[0][CindexNum]
+		imageName = images[1][int(CindexNum)][random.randint(0, len(images[1][CindexNum]) - 1)]
+	
+	sendimage = "./ImagePool/" + category + '/' + imageName
+	print(sendimage)
+	await client.send_file(ctx.message.channel, sendimage)
+
+
+#Client Token (Discord App Token)
+client.run('XXX')
