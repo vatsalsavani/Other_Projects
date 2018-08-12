238b275eebd3bc970085c3b7228581afe9d4a9a8
vatsalsavani <vatsalsavani7@gmail.com>
2018-08-12 21:25:42 +0800

    Iris Flower Classificatin

---
+++ AI_IrisFlowerClassification/irisFlowerClassification.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+#Parameters
+learningRate = 0.1
+
+
+#Network Parameter
+hiddenLayer1 = 3 #Number of neurons in hidden layer 1
+hiddenLayer2 = 3 #Number of neurons in hidden layer 2
+numInputs = 4 #Number of inputs
+numOutputs = 3 #Number of outputs
+
+#Weights and Biases
+weights = {
+    'hL1': tf.Variable (tf.random_normal ([numInputs, hiddenLayer1])),
+    'hL2': tf.Variable (tf.random_normal ([hiddenLayer1, hiddenLayer2])),
+    'output': tf.Variable (tf.random_normal ([hiddenLayer2, numOutputs]))
+}
+biases = {
+    'hL1': tf.Variable (tf.random_normal ([hiddenLayer1])),
+    'hL2': tf.Variable (tf.random_normal ([hiddenLayer2])),
+    'output': tf.Variable (tf.random_normal ([numOutputs]))
+}
+
+def nn (x):
+    
